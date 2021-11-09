25fe1cd7ab5584b8f0f36d50de1fb3b9024288f7
Jérémy <jeremy.pro.lp@gmail.com>
2021-11-09 23:15:18 +0100

    init

---
+++ src/Entity/Post.php
@@ -0,0 +1,196 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PostRepository::class)
+ */
+class Post
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private string $slug;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private string $title;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private string $description;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private string $body;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private string $author;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private string $mainImage;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private \DateTimeImmutable $publishedAt;
+
+    /**
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->publishedAt = new \DateTimeImmutable();
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    /**
+     * @param string $slug
+     * @return Post
+     */
+    public function setSlug(string $slug): Post
+    {
+        $this->slug = $slug;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return Post
+     */
+    public function setTitle(string $title): Post
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     * @return Post
+     */
+    public function setDescription(string $description): Post
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBody(): string
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param string $body
+     * @return Post
+     */
+    public function setBody(string $body): Post
+    {
+        $this->body = $body;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthor(): string
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param string $author
+     * @return Post
+     */
+    public function setAuthor(string $author): Post
+    {
+        $this->author = $author;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMainImage(): string
+    {
+        return $this->mainImage;
+    }
+
+    /**
+     * @param string $mainImage
+     * @return Post
+     */
+    public function setMainImage(string $mainImage): Post
+    {
+        $this->mainImage = $mainImage;
+        return $this;
+    }
+
+    /**
+     * @return \DateTimeImmutable
+     */
+    public function getPublishedAt(): \DateTimeImmutable
+    {
+        return $this->publishedAt;
+    }
+
+    /**
+     * @param \DateTimeImmutable $publishedAt
+     * @return Post
+     */
+    public function setPublishedAt(\DateTimeImmutable $publishedAt): Post
+    {
+        $this->publishedAt = $publishedAt;
+        return $this;
+    }
+}
